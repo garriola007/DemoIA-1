d1c0282d2abae106788e7d6561786448942d3d24
garriola007 <gustavo.arriola@gmail.com>
2024-08-30 16:12:53 -0300

    codigo

--- app.py
+++ app.py
@@ -7,3 +7,23 @@ data = pd.read_csv("content/gastronimiaCABA.csv")
 data.head()
 
 st.dataframe(data)
+
+from dotenv import load_dotenv
+load_dotenv()
+
+GROQ_API_KEY = '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'
+
+from langchain_groq.chat_models import ChatGroq
+
+llm = ChatGroq(
+    model_name = "mixtral-8x7b-32768",
+    temperature = 0,
+    max_tokens = 1000,
+    api_key=GROQ_API_KEY
+)
+
+from pandasai import SmartDataframe
+
+df = SmartDataframe(data, config={"llm": llm})
+
+st.write(df.chat("cuenta la cantidad de registros"))
